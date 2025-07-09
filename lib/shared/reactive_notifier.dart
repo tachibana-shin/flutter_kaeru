6d3fefc9528f68a2906dc647b65841dc5437dfad
tachib.shin <tachib.shin@gmail.com>
2025-07-09 15:55:39 +0000

    feat: add mounted check to ReactiveNotifier

--- lib/shared/reactive_notifier.dart
+++ lib/shared/reactive_notifier.dart
@@ -3,6 +3,9 @@ import 'package:flutter/foundation.dart';
 import '../foundation/computed.dart';
 
 abstract class ReactiveNotifier<T> extends ChangeNotifier {
+  bool _mounted = true;
+  get mounted => _mounted;
+
   T get value;
 
   Computed<U> select<U>(U Function(T value) getter) {
@@ -27,4 +30,15 @@ abstract class ReactiveNotifier<T> extends ChangeNotifier {
   // void dispose() {
   //   _listeners.clear();
   // }
+
+  @override
+  void notifyListeners() {
+    if (mounted) super.notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _mounted = false;
+    super.dispose();
+  }
 }
